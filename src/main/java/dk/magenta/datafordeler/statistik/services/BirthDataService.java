bd635a0e084b909056f759f70e2cbf0cc9ae8d53
Lars Peter Thomsen <larsp@magenta.dk>
2018-04-24 14:21:40 +0200

    Add floor data

--- src/main/java/dk/magenta/datafordeler/statistik/services/BirthDataService.java
+++ src/main/java/dk/magenta/datafordeler/statistik/services/BirthDataService.java
@@ -197,6 +197,7 @@ public class BirthDataService extends StatisticsService {
                         item.put(prefix + MUNICIPALITY_CODE, addressData.getMunicipalityCode() );
                         item.put(prefix + ROAD_CODE, formatRoadCode(addressData.getRoadCode()));
                         item.put(prefix + HOUSE_NUMBER, formatHouseNnr(addressData.getHouseNumber()));
+                        item.put(prefix + FLOOR_NUMBER, addressData.getFloor());
                         item.put(prefix + DOOR_NUMBER, addressData.getDoor());
                         item.put(prefix + BNR, formatBnr(addressData.getBuildingNumber()));
 
