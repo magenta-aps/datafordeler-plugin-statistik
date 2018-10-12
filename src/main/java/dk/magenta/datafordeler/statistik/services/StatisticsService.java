9be8f00b2ee9fc2c00d22a002b4eb3656f069ad1
Lars Peter Thomsen <larsp@magenta.dk>
2018-10-12 11:41:27 +0200

    Convert date for lookup

--- src/main/java/dk/magenta/datafordeler/statistik/services/StatisticsService.java
+++ src/main/java/dk/magenta/datafordeler/statistik/services/StatisticsService.java
@@ -467,4 +467,11 @@ public abstract class StatisticsService {
         return time.format(dmyFormatter);
     }
 
+    public static LocalDate convertDate(OffsetDateTime date) {
+        if (date != null) {
+            return date.atZoneSameInstant(timezone).toLocalDate();
+        }
+        return null;
+    }
+
 }
