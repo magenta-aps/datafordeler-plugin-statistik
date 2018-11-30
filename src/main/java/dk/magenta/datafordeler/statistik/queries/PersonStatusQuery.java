e2d40c4565796b36673463c2697fb1adc6d68263
Lars Peter Thomsen <larsp@magenta.dk>
2018-11-30 13:03:28 +0100

    Avoid undone entries

--- src/main/java/dk/magenta/datafordeler/statistik/queries/PersonStatusQuery.java
+++ src/main/java/dk/magenta/datafordeler/statistik/queries/PersonStatusQuery.java
@@ -27,18 +27,27 @@ public class PersonStatusQuery extends PersonStatisticsQuery {
         lookupDefinition.setMatchNulls(true);
 
         ArrayList<FieldDefinition> fieldDefinitions = new ArrayList<>();
-        fieldDefinitions.add(new FieldDefinition(
+        FieldDefinition addressDefinition = new FieldDefinition(
                 PersonEntity.DB_FIELD_ADDRESS + LookupDefinition.separator + AddressDataRecord.DB_FIELD_MUNICIPALITY_CODE,
                 900,
                 Integer.class,
                 LookupDefinition.Operator.GTE
-        ));
-        fieldDefinitions.add(new FieldDefinition(
+        );
+        addressDefinition.and(PersonEntity.DB_FIELD_ADDRESS + LookupDefinition.separator + AddressDataRecord.DB_FIELD_UNDONE,
+                false,
+                Boolean.class,
+                LookupDefinition.Operator.EQ);
+
+        fieldDefinitions.add(addressDefinition);
+
+
+        FieldDefinition statusDefinition = new FieldDefinition(
                 PersonEntity.DB_FIELD_STATUS + LookupDefinition.separator + PersonStatusDataRecord.DB_FIELD_STATUS,
                 90,
                 Integer.class,
                 LookupDefinition.Operator.NE
-        ));
+        );
+        fieldDefinitions.add(statusDefinition);
 
         for (FieldDefinition fieldDefinition : fieldDefinitions) {
             applyRegistrationTimes(fieldDefinition);
