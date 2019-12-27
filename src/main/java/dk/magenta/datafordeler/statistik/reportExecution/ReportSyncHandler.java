66aa278f17046e65fc2ed5aafb09fde478a6c2c0
madsmj <madsmj@gmail.com>
2019-12-27 14:08:10 +0100

    adding check for undone reports

--- src/main/java/dk/magenta/datafordeler/statistik/reportExecution/ReportSyncHandler.java
+++ src/main/java/dk/magenta/datafordeler/statistik/reportExecution/ReportSyncHandler.java
@@ -12,6 +12,9 @@ import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Handler for objects that keeps track of running reports
+ */
 public class ReportSyncHandler {
 
     private Logger log = LogManager.getLogger(ReportSyncHandler.class);
@@ -25,7 +28,12 @@ public class ReportSyncHandler {
         return this.session;
     }
 
-
+    /**
+     * Create a reportstatus object, reject if there is any which is not done
+     * @param reportStatusObject
+     * @return
+     * @throws Exception
+     */
     public boolean createReportStatusObject(ReportAssignment reportStatusObject) throws Exception {
         CriteriaBuilder builder = session.getCriteriaBuilder();
         CriteriaQuery<ReportAssignment> criteria = builder.createQuery(ReportAssignment.class);
@@ -46,8 +54,25 @@ public class ReportSyncHandler {
         return true;
     }
 
+    public boolean hasUndoneReportsStatus() {
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<ReportAssignment> criteria = builder.createQuery(ReportAssignment.class);
+        Root<ReportAssignment> page = criteria.from(ReportAssignment.class);
+        criteria.select(page);
+        criteria.where(builder.notEqual(page.get(ReportAssignment.DB_FIELD_REPORT_STATUS), ReportProgressStatus.done));
+
+        TypedQuery<ReportAssignment> query = session.createQuery(criteria);
+        query.setHint(QueryHints.HINT_CACHEABLE, true);
+
+        return query.getResultList().size()>0;
+    }
 
 
+    /**
+     * Set a new status to a report, it is invalid to change status on a failed report
+     * @param reportUuid
+     * @param status
+     */
     public void setReportStatus(String reportUuid, ReportProgressStatus status) {
 
         session.beginTransaction();
@@ -71,7 +96,12 @@ public class ReportSyncHandler {
         session.getTransaction().commit();
     }
 
-
+    /**
+     * Get a list of reports attached to a specified collectionUuid
+     * @param collectionUuid
+     * @param reportProgressStatus
+     * @return
+     */
     public List<String> getReportList(String collectionUuid, ReportProgressStatus reportProgressStatus) {
 
         ArrayList<String> reports = new ArrayList<String>();
